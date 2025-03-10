2da1d63d2a9acf9bd7f1b7b3b7f20709a53420f5
alessandra120 <alessantos9271123@gmail.com>
2025-03-10 19:12:36 +0000

    Atividade

--- 3.exercícios/13.atividade.py
+++ 3.exercícios/13.atividade.py
@@ -1,11 +1,31 @@
 import os
 os.system ("clear")
+#Solicite que o usuário informe o valor de um produto e a forma de pagamento.
+#1 - Pagamento à vista;
+#2 - Pagamento à prazo.
+#Se o produto for pago à vista aplique um desconto de 10% antes de mostrar o valor final, senão informe o mesmo
+#valor do produto.
+#Se for escolhida a opção de pagamento à prazo, solicite que o usuário digite a quantidade de parcelas que ele
+#deseja pagar. Podendo parcelar em até 6 vezes.
+#No final, mostre:
+#Se o pagamento for à vista: 
+#	Valor do produto: R$ 100,00
+#Forma de pagamento: à vista
+#Valor do desconto: R$ 10,00
+#Total a pagar: R$ 90,00
+#Se o pagamento for à prazo:
+#Valor do produto: R$ 100,00
+#Forma de pagamento: à prazo
+#Quantidade de parcelas: 6
+#Valor por parcela: R$ 16,66
+#Total à prazo: R$ 100,00
+
 
 valor_do_produto = float(input("Digite o valor do produto: "))
 forma_de_pagamento = int(input("""
 1 - A vista
 2 - Parcelado                                                             
-Digite a forma de pamento: """))
+Digite a forma de pagamento: """))
 
 match forma_de_pagamento:
     case 1:
@@ -19,12 +39,8 @@ match forma_de_pagamento:
         3 - parcelado = valor_do_produto / 4
         4 - parcelado = valor_do_produto / 5
         5 - parcelado = valor_do_produto / 6
-        6 - parcelado = valor_do_produto / 7
-        7 - parcelado = valor_do_produto / 8
-        8 - parcelado = valor_do_produto / 9
-        9 - parcelado = valor_do_produto / 10
     case 4:
         1 - ("Vo")
 
-    print("f\n")
+    print(f"\n")
 
