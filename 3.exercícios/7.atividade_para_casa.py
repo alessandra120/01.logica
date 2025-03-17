f3b749d58b59c39515f873ebcf5f0c64cff2d97f
alessandra120 <alessantos9271123@gmail.com>
2025-03-17 20:07:25 +0000

    Aula laço de repetição

--- 3.exercícios/7.atividade_para_casa.py
+++ 3.exercícios/7.atividade_para_casa.py
@@ -1,16 +1,20 @@
 #ATIVIDADE PARA CASA
 #ELABORE UM ALGORITMO USANDO OPERAÇÕES PARA INFORMAR SE UMA PESSOA É OBRIGADA A VOTAR.
-#CONSIDERE QUE A REGRA É QUE MENORES E MAIORES QUE 65 NÃO SÃO OBRIGADOS A VOTAR.
+#CONSIDERE QUE A REGRA É QUE MENORES DE 18 E MAIORES QUE 65 NÃO SÃO OBRIGADOS A VOTAR.
 
 import os
 os.system ("clear")
 
 # ENTRADA
-idade int(input("Digite sua idade: "))
+idade = int(input("Digite sua idade: "))
 
-# PROCESSAMENTO
-# OPÇÃO 1.
-# IF IDADE < 18 OR 
+if idade < 65:
+    R = "Não são obrigados a votar"
+if idade > 18:
+    R = "Não são obrigados a votar"
+else:
+    R = "São obrigados a votar"
+
 
 
 
